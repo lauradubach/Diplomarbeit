95d513c1174eb5f2bd09b33c88aad06fb817c563
lauradubach <laura.dubach@edu.tbz.ch>
2026-09-22 16:18:58 +0200

    fix(cost-mgmt): robustes PowerShell-Skript für Cost Management Query API

--- analyse/cost-management-query-test.ps1
+++ analyse/cost-management-query-test.ps1
@@ -1,20 +1,27 @@
+$subId = "9d581cdd-d85b-43f4-8b5a-238f5d03b50c"
+
 $token = az account get-access-token --resource https://management.azure.com --query accessToken -o tsv
 
 $body = @'
 {
-  "type": "ActualCost",
-  "timeframe": "MonthToDate",
-  "dataset": {
-    "granularity": "Daily",
-    "aggregation": { "totalCost": { "name": "PreTaxCost", "function": "Sum" } },
-    "grouping": [{ "name": "ResourceId", "type": "Dimension" }]
-  }
+"type": "ActualCost",
+"timeframe": "MonthToDate",
+"dataset": {
+"granularity": "Daily",
+"aggregation": { "totalCost": { "name": "PreTaxCost", "function": "Sum" } },
+"grouping": [{ "name": "ResourceId", "type": "Dimension" }]
+}
 }
 '@
 
-Invoke-RestMethod `
-  -Uri "https://management.azure.com/subscriptions/0576f223-f60a-4e64-839e-066b2558a5ec/providers/Microsoft.CostManagement/query?api-version=2025-03-01" `
-  -Method POST `
-  -Headers @{ Authorization = "Bearer $token"; ClientType = "dip-diplomarbeit" } `
-  -ContentType "application/json" `
-  -Body $body
+$headers = @{
+Authorization = "Bearer $token"
+ClientType = "dip-diplomarbeit"
+}
+
+$uri = "https://management.azure.com/subscriptions/$subId/providers/Microsoft.CostManagement/query?api-version=2025-03-01"
+
+$result = Invoke-RestMethod -Uri $uri -Method POST -Headers $headers -ContentType "application/json" -Body $body
+
+$result.properties.columns | ConvertTo-Json -Depth 10
+$result.properties.rows | ConvertTo-Json -Depth 10
